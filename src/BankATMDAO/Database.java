4432311237d2f6a88d757c088d4f4460afdafc14
bofeng <bournafe@gmail.com>
2020-04-27 01:50:01 -0400

    Create tables (user, accounts)

--- src/BankATMDAO/Database.java
+++ src/BankATMDAO/Database.java
@@ -6,6 +6,7 @@ public class Database {
 
 	static final String JDBC = "com.mysql.cj.jdbc.Driver";
 	static final String URL = "jdbc:mysql://localhost?useSSL=false&serverTimezone=EST";
+	static final String URL_DB = "jdbc:mysql://localhost:3306/bankdb?useSSL=false&serverTimezone=EST";
 	/*
 	 * MySQL
 	 *   - root
@@ -40,16 +41,51 @@ public class Database {
 			 * If the bankdb exists, just use it
 			 * Else, create a bankdb
 			 */
+			
 			if (dbExist) {
 				// 
 			}
 			else {
 				System.out.println("There is no bankdb db!");
 				stmt = conn.createStatement();
-				
-				String sql = "create database bankdb";
-				stmt.executeUpdate(sql);
+				/*
+				 * Create a database called "bankdb"
+				 */
+				String sql_1 = "create database bankdb";
+				stmt.executeUpdate(sql_1);
 				stmt.close(); // If dbExist, stmt is null and doesn't need to be closed
+				
+				Connection connDB = DriverManager.getConnection(URL_DB, USERNAME, PASSWORD);;
+				Statement stmtDB = connDB.createStatement();
+				/*
+				 * Create table "users"
+				 */
+				String sql_2 = "create table users (" +
+						       "username varchar(255) not null, " +
+						       "password varchar(255) not null, " +
+						       "email varchar(255) not null, " + 
+						       "primary key( username ))";
+				stmtDB.executeUpdate(sql_2);
+				/*
+				 * Create table ""
+				 */
+				String sql_3 = "create table accounts (" +
+					           "username varchar(255) not null, " +
+					           "accountnumber varchar(255) not null, " +
+					           "balance double, " + 
+					           "primary key( accountnumber ))";
+				stmtDB.executeUpdate(sql_3);
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
 			}
 			conn.close();
 		} catch (Exception e) {
