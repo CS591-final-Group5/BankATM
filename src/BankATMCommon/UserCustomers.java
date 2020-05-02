b88a7f96451af0c6ab28d27e518c52eb4e63664a
Bobbcat2 <58884859+Bobbcat2@users.noreply.github.com>
2020-05-02 17:33:00 -0400

    Update UserCustomers.java
    Update

--- src/BankATMCommon/UserCustomers.java
+++ src/BankATMCommon/UserCustomers.java
@@ -3,14 +3,15 @@ package BankATMCommon;
 
 import java.util.ArrayList;
 
-public class UserCustomers extends Users {
+public class UserCustomers extends Users implements DailyReport {
 	
 	private ArrayList<Accounts> alAccounts;
-	private Transactions transactions;
+	private ArrayList<Transactions> transactions;
 	
 	public UserCustomers(String Username, String Password, String Email, String Name) {
 		super(Username, Password, Email, Name);
-		alAccounts = new ArrayList<Accounts> ();
+		alAccounts = new ArrayList<Accounts>();
+		transactions = new ArrayList<Transactions>();
 	}
 	
 	public ArrayList<Accounts> getAccounts() {
@@ -21,4 +22,21 @@ public class UserCustomers extends Users {
 		alAccounts.add(account);
 	}
 	
+	public ArrayList<Transactions> getTransactions() {
+		return transactions;
+	}
+	
+	public addNewTransactions(Transactions transaction) {
+		transactions.add(transaction);
+	}
+	
+	public dailyReport(Date time) {
+		ArrayList<Transactions> report = new ArrayList<Transactions>()
+		for(int i = 0; i < transactions.size(); i++) {
+			if (transactions.get(i).getTime().equals(time)) {
+				report.add(transactions.get(i));
+			}
+		}
+		return report;
+	}
 }
